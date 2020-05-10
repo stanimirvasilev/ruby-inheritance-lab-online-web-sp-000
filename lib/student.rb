e1a33c5aa81777a8b2c9eb124fc8fc22271ef36a
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-05-10 12:51:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,17 @@
 require_relative "./user.rb"
 class Student < User
 
+def initialize
+  @knowledge = []
+end
 
 
 def learn(string_knowledge)
   @knowledge << string_knowledge
-  
 end
 
 def knowledge
   @knowledge 
-  
 end
 
 
